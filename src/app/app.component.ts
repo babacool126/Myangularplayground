ca9ca821d7a2dbe4c316749e31f60f70d556e3ce
babacool126 <babacool126@gmail.com>
2023-08-07 20:15:08 +0200

    first test with country and cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Country } from './country';
+import { City } from './city';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Countries and Cities</h1>
+  <ul>
+    <li *ngFor="let country of countries">
+      {{ country.name }} (Capital: {{ country.capital }}) - Population: {{ country.population }}
+      <ul>
+        <li *ngFor="let city of getCitiesByCountry(country.name)">
+          {{ city.name }} - Population: {{ city.population }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+`
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  countries: Country[] = [
+    {
+      name: 'Contry 1',
+      capital: 'Capital 1',
+      population: 1000000,
+    }
+  ];
+
+  ciities: City[] = [
+    {
+      name: 'City 1',
+      population: 500000,
+      country: 'Country 1',
+    }
+  ];
+
+  getCitiesByCountry(countryName: string): City[] {
+    return this.cities.filter((city) => city.country === countryName);
+  }
 }
